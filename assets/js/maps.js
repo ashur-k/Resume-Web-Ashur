5113ca62e43ac96d5c819b9e423cebae33e3ed3b
ashur-k <ashurkanwal@yahoo.com>
2020-03-31 08:35:23 +0000

    reverting changes

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,6 @@
   function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
+                ZOOM: 3,
                 zoom: 2,
                 center: {
                     lat: 46.619261,
