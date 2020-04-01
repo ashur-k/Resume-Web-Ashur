084c49fb5545fefb322a2758c1feec9cc8cf2748
ashur-k <ashurkanwal@yahoo.com>
2020-04-01 12:03:25 +0000

    Map coordinates updated

--- assets/js/mapsci.js
+++ assets/js/mapsci.js
@@ -2,17 +2,19 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
+            lat: 55.3781,
+            lng: 3.4360,
+            
+                 },
+               
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 24.8387, lng: 67.1209},
+        { lat: 54.5705, lng: 1.3290},
+        { lat: 53.2982, lng: 6.1788}
     ];
 
     var markers = locations.map(function(location, i) {
