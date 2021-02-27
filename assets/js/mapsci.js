17c0950b849f488bad99d1ccb4fbfc5f6bed7c04
ashur-k <ashurkanwal@yahoo.com>
2021-02-27 17:53:46 +0000

    worked on site to make it more consistent

--- assets/js/mapsci.js
+++ assets/js/mapsci.js
@@ -2,8 +2,8 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 55.3781,
-            lng: 3.4360,
+            lat:56.007120,
+            lng:-3.709860
             
                  },
                
@@ -12,9 +12,10 @@ function initMap() {
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 24.8387, lng: 67.1209},
-        { lat: 54.5705, lng: 1.3290},
-        { lat: 53.2982, lng: 6.1788}
+        {lat:53.300415, lng:-6.17841},
+        {lat:54.56224, lng:-1.30013},
+        {lat:24.86678, lng:67.031129},
+    
     ];
 
     var markers = locations.map(function(location, i) {
